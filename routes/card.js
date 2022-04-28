0229cd23cde525103a98c9158517f7e492c4e6db
MantillaBigfoot <lotz.christoph@outlook.com>
2022-04-28 21:19:28 +0200

    add card/create GET/POST

--- routes/card.js
+++ routes/card.js
@@ -1,3 +1,34 @@
 const express = require('express');
+const Card = require('./../models/card');
 const routeGuard = require('./../middleware/route-guard');
+const fileUpload = require('./../middleware/file-upload');
 const cardRouter = new express.Router();
+
+cardRouter.get('/create', routeGuard, (req, res) => {
+  res.render('/card/create');
+});
+
+cardRouter.post(
+  '/create',
+  routeGuard,
+  fileUpload.single('picture'),
+  (req, res, next) => {
+    const { title, text } = req.body;
+    let media;
+    if (req.file) {
+      media = req.file.path;
+    }
+    Card.create({
+      title,
+      media,
+      text,
+      creator: req.user._id
+    })
+      .then(() => {
+        res.redirect('/');
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+);
