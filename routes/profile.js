c1d560216b5ca46f438858599d33dc2660f582b3
unknown <aldevtostudio@gmail.com>
2022-04-28 21:21:21 +0200

    Fixed profile router

--- routes/profile.js
+++ routes/profile.js
@@ -4,4 +4,4 @@ const express = require('express');
 const routeGuard = require('./../middleware/route-guard');
 const profileRouter = new express.Router();
 
-module.exports = router;
+module.exports = profileRouter;
