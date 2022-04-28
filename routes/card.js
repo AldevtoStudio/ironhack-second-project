8ec8a8020734ed67b080b12cf05a538c533fd236
Arundathi Jayarathne <sandyjayarathne@gmail.com>
2022-04-28 21:01:45 +0200

    changes

--- routes/card.js
+++ routes/card.js
@@ -1,3 +1,23 @@
 const express = require('express');
+const Feedback = require('../models/feedback');
 const routeGuard = require('./../middleware/route-guard');
+const Card = require('./../models/card');
 const cardRouter = new express.Router();
+
+cardRouter.post('/:id/delete', (req, res) => {
+  const { id } = req.params;
+  Card.findOneAndDelete({ _id: id, creator: req.user._id }).then(() => {
+    res.redirect('profile');
+  });
+});
+
+cardRouter.post('/:id/like', (req, res, next) => {
+  const { id } = req.params;
+  Feedback.findOne({ card: id, user: req.user._id }).then((like) => {
+    if (like) {
+      throw new Error('');
+    }
+  });
+});
+
+module.exports = cardRouter;
