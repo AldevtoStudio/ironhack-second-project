4ae047a57d8244e57087b3784130540fd9e65cfa
MantillaBigfoot <lotz.christoph@outlook.com>
2022-05-07 10:33:56 +0200

    add styles

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -7,11 +7,37 @@ const leaderboardRouter = new express.Router();
 const User = require('./../models/user');
 
 leaderboardRouter.get('/', (req, res, next) => {
+  let firstUser = {};
+  let secondUser = {};
+  let thirdUser = {};
+  let currentUSer = {};
+
   User.find()
     .sort({ totalScore: -1 })
     .then((users) => {
-      console.log(users);
-      res.render('leaderboard', { users });
+      users.forEach((value, index) => {
+        console.table('user' + value);
+        console.log(index);
+        if (index === 0) {
+          firstUser.name = value.name;
+          firstUser.score = value.totalScore;
+        }
+        if (index === 1) {
+          secondUser.name = value.name;
+          secondUser.score = value.totalScore;
+        }
+        if (index === 2) {
+          thirdUser.name = value.name;
+          thirdUser.score = value.totalScore;
+        }
+      });
+      console.log(firstUser);
+      console.log(secondUser);
+      console.log(thirdUser);
+      res.render('leaderboard', {
+        users,
+        pageStyles: [{ style: '/styles/leaderboard.css' }]
+      });
     })
     .catch((err) => {
       next(err);
