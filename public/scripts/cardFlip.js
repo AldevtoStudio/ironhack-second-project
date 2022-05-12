830ffcc3100b96bd2f8cca320d3cd72555900f0e
unknown <aldevtostudio@gmail.com>
2022-05-13 00:06:24 +0200

    Fixed card flip

--- public/scripts/cardFlip.js
+++ public/scripts/cardFlip.js
@@ -1,12 +1,26 @@
 let card = document.querySelector('.cardFlip');
 let btns = document.querySelectorAll('.commentIcon');
+let firstTime = true;
+let start = null;
+
+window.requestAnimationFrame((timestamp) => {
+  if (!start) start = timestamp;
+  let progress = timestamp - start;
+
+  if (card.classList.contains('is-flipped') && firstTime)
+    card.classList.remove('is-flipped');
+
+  if (progress >= 150 && firstTime) {
+    card.classList.toggle('is-flipped');
+    let firstTime = false;
+  }
+});
 
 if (card.classList.contains('is-flipped')) {
-  card.classList.remove('is-flipped');
-  window.setTimeout(() => {
-    card.classList.toggle('is-flipped');
-  }, 150);
+  window.setTimeout(() => {}, 150);
 }
+
+const firstTimeFlip = () => {};
 
 btns.forEach((btn) => {
   btn.addEventListener('click', function () {
