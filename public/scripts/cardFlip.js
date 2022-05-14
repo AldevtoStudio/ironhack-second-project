8069feb71f8a4cf58aa48553f6df2eef79c6ee7b
unknown <aldevtostudio@gmail.com>
2022-05-14 11:57:22 +0200

    Flip animation fixed

--- public/scripts/cardFlip.js
+++ public/scripts/cardFlip.js
@@ -1,16 +1,16 @@
-let card = document.querySelector('.cardFlip');
-let btns = document.querySelectorAll('.commentIcon');
+window.addEventListener('load', () => {
+  let card = document.querySelector('.cardFlip');
+  let btns = document.querySelectorAll('.commentIcon');
 
-if (card.classList.contains('is-flipped')) {
-  card.classList.remove('is-flipped');
+  if (card.classList.contains('firstFlip')) {
+    setTimeout(() => {
+      card.classList.toggle('is-flipped');
+    }, 300);
+  }
 
-  setTimeout(() => {
-    card.classList.toggle('is-flipped');
-  }, 300);
-}
-
-btns.forEach((btn) => {
-  btn.addEventListener('click', function () {
-    card.classList.toggle('is-flipped');
+  btns.forEach((btn) => {
+    btn.addEventListener('click', function () {
+      card.classList.toggle('is-flipped');
+    });
   });
 });
